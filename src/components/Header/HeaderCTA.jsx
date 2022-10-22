fb12249acec8f27277710ee0ca19039c9005e4dc
matheussdev3@gmail.com <matheussdev3@gmail.com>
2022-10-22 20:03:06 -0300

    last commit

--- src/components/Header/HeaderCTA.jsx
+++ src/components/Header/HeaderCTA.jsx
@@ -1,9 +1,10 @@
 import "./Header.css";
+import CV from "/assets/CV.pdf"
 
 export const HeaderCTA = () => {
   return (
     <div className="cta">
-      <a href="" download className="btn">
+      <a href={CV} download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
